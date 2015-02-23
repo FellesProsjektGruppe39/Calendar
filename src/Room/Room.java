d6dbd7b898b8ef790fc4c9a8cd19465dea880a70
davidsl <davidsl@stud.ntnu.no>
2015-02-23 14:33:08 +0100

    La til Romklasse

---
+++ src/Room/Room.java
@@ -0,0 +1,15 @@
+package Room;
+
+public class Room {
+	
+	private int Roomnumber;
+	
+	private int getRoomnumber(){
+		return Roomnumber;
+	}
+	
+	private void setRoomnumber(int roomnum){
+		Roomnumber = roomnum;
+	}
+
+}
