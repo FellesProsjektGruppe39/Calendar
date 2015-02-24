ec6df76c1dc7bf78f87dee2217e7d679abb2c167
kimbim <KimBim@dhcp-026252.wlan.ntnu.no>
2015-02-24 15:53:46 +0100

    nesten ferdig

--- src/Room/EditRoom.java
+++ src/Room/EditRoom.java
@@ -1,5 +1,43 @@
 package Room;
 
-public class EditRoom {
+import mysql.sqlExecute;
 
+public class EditRoom {
+	
+	sqlExecute sql = new sqlExecute();
+	
+	private static String romNavn;
+	
+	EditRoom(String romNavn) {
+		this.romNavn = romNavn;
+	}
+	
+	public void endreNavn(String nyttNavn) {
+		
+		sql.execute("UPDATE rom SET romnavn =" + "\"" + nyttNavn + "\"" + "WHERE romnavn =" + romNavn);
+		
+	}
+	
+	public void endreKapasitet(int nyKapasitet) {
+		sql.execute("UPDATE rom SET Kapasitet =" + "\"" + nyKapasitet + "\"" + "WHERE romnavn ='" + romNavn + "'");
+
+	}
+	
+	public void endreSted(String nyttSted) {
+		sql.execute("UPDATE rom SET Sted =" + "\"" + nyttSted + "\"" + "WHERE romnavn ='" + romNavn + "'");
+
+	}
+	
+	public void endreBeskrivelse(String nyBeskrivelse) {
+		
+		sql.execute("UPDATE rom SET Beskrivelse =" + "\"" + nyBeskrivelse + "\"" + "WHERE romnavn ='" + romNavn + "'");
+	}
+	
+	public static void main(String[] args) {
+		EditRoom rom = new EditRoom("Galtvort");
+		
+		rom.endreBeskrivelse("Morradiiiiiiii");
+		rom.endreKapasitet(10);
+		rom.endreSted("Hvoooor");
+	}
 }
