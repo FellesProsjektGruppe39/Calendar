e6277186ec880f2eea30913298ded5c96e37d406
martinrholth <martinr.holth@gmail.com>
2015-02-24 15:42:21 +0100

    Merge DeleteUser and CreateUser to User

--- src/User/User.java
+++ src/User/User.java
@@ -1,23 +1,69 @@
 package User;
 
+import mysql.sqlExecute;
+import mysql.sqlRetrieve;
 public class User {
-
-	private String Name;
 	
+	static int brukerid;
+	static String fornavn;
+	static String etternavn;
+	static int TlfNr;
+	static String stilling;
+	static String passord;
+	static int admin;
+	static String brukernavn;
+	sqlExecute create = new sqlExecute();
 	
-	
-	
-	public String getName() {
-		return this.Name;
+	User(int brukerid, String fornavn, String etternavn, int TlfNr,
+			String brukernavn, String passord, String stilling, int admin){
+		this.brukerid=brukerid;
+		this.fornavn=fornavn;
+		this.etternavn=etternavn;
+		this.TlfNr=TlfNr;
+		this.brukernavn=brukernavn;
+		this.passord=passord;
+		this.stilling=stilling;
+		this.admin=admin;
+		
 	}
-	public void setName(String name) {
-		this.Name = name;
+	User(int brukerid){
+		this.brukerid=brukerid;
 	}
+	User(int brukerid, String brukernavn, String passord){
+		this.brukerid=brukerid;
+		this.brukernavn=brukernavn;
+		this.passord=passord;
+	}
+		 public void CreateUser(){
+		try{
+			if(this.brukernavn==null||this.passord==null){
+				throw new IllegalArgumentException("Missing username and password");
+				
+			}
+		create.execute("INSERT INTO bruker (brukerid,fornavn,etternavn,tlfnr,"
+				+ " brukernavn, passord, stilling, admin) VALUES ('" + this.brukerid +
+				"','" + this.fornavn + "','" + this.etternavn + "','" 
+	            + this.TlfNr + "','" + this.brukernavn + "','" + this.passord + "','"  +
+				this.stilling + "','" + this.admin + "')");
+		
+		}
+		finally{}
+		
+	}
+	public void DeleteUser(){
+			
+		create.execute("DELETE FROM bruker WHERE brukerid =" + this.brukerid);
+	}
+	
+	
 	
 	public static void main(String[] args) {
 		
-		CreateUser User = new CreateUser(101, "Dake", "Persson", 78978978, "NRK", "perp", "Daglig leder", 0);
-		
+		User User_1 = new User(103);
+		User_1.DeleteUser();
+	
+		//DeleteUser UserDel=new DeleteUser(100);
+		//UserDel.delete();
 	}
 
 }
