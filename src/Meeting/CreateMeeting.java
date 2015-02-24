f599811e2634ded72b10810ef733cd1f5706c1e5
davidsl <davidsl@stud.ntnu.no>
2015-02-24 14:02:02 +0100

    CreateMeeting startup

--- src/Meeting/CreateMeeting.java
+++ src/Meeting/CreateMeeting.java
@@ -1,5 +1,23 @@
 package Meeting;
 
+import java.util.Date;
+
 public class CreateMeeting {
+	
+	private int Meetingtid;
+	private Date starttid;
+	private Date slutttid;
+	private String Beskrivelse;
+	
+	CreateMeeting(int Meetingid, Date starttid, Date slutttid, String Beskrivelse){
+		this.Meetingtid = Meetingid;
+		this.starttid = starttid;
+		this.slutttid = slutttid;
+		this.Beskrivelse = Beskrivelse;
+	}
+	
+	private void create(){
+		
+	}
 
 }
