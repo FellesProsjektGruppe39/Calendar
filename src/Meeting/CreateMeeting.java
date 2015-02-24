98584b8fbd957d6cd9c993d73ff8260263a64f0a
davidsl <davidsl@stud.ntnu.no>
2015-02-24 15:23:29 +0100

    createMeeting jobbing

--- src/Meeting/CreateMeeting.java
+++ src/Meeting/CreateMeeting.java
@@ -1,23 +1,27 @@
 package Meeting;
 
 import java.util.Date;
+import mysql.sqlExecute;
 
 public class CreateMeeting {
 	
-	private int Meetingtid;
+	private int Meetingid;
 	private Date starttid;
 	private Date slutttid;
 	private String Beskrivelse;
 	
 	CreateMeeting(int Meetingid, Date starttid, Date slutttid, String Beskrivelse){
-		this.Meetingtid = Meetingid;
+		this.Meetingid = Meetingid;
 		this.starttid = starttid;
 		this.slutttid = slutttid;
 		this.Beskrivelse = Beskrivelse;
 	}
 	
 	private void create(){
-		
+		sqlExecute cre = new sqlExecute();
+		cre.execute("INSERT INTO møte (møteid,starttidspunkt,sluttidspunkt,beskrivelse) VALUES ('" + Meetingid +
+				"','" + starttid + "','" + slutttid + "','" 
+	            + Beskrivelse + "')");
 	}
 
 }
