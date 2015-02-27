27cec321138db876dc61ec35752d3ca96a2bb68c
davidsl <davidsl@stud.ntnu.no>
2015-02-27 09:37:31 +0100

    Jobb med Main og LogIn

--- src/logIn/LogIn.java
+++ src/logIn/LogIn.java
@@ -8,11 +8,13 @@ import mysql.sqlRetrieve;
 
 public class LogIn {
 	
+	
+	private int bruker;
 	private String username;
 	private String password;
 	private String CorrectPassword;
 	
-	public LogIn()  {
+	public int LogIn()  {
 		toString2();
 		
 		sqlRetrieve passord = new sqlRetrieve(("SELECT passord FROM bruker WHERE brukernavn =" + "\"" + username +"\""));		
@@ -21,9 +23,14 @@ public class LogIn {
 		
 		if (CorrectPassword.equals((password))){
 			System.out.println(toString1());
+			sqlRetrieve brukerid = new sqlRetrieve(("SELECT brukerid FROM bruker WHERE brukernavn =" + "\"" + username +"\""));
+			bruker = Integer.parseInt(brukerid.getQuery()[0][0]);
+			return bruker;
+			
 			
 		}else
 			System.out.println("Du feilet");
+			return -1;
 		
 	}
 	
