623900e1cbfe6de2e9283e7f7f912ce8eb736c4a
martinrholth <martinr.holth@gmail.com>
2015-03-02 14:55:00 +0100

    Fix CreateCalendar minus room update

--- src/Meeting/CreateMeeting.java
+++ src/Meeting/CreateMeeting.java
@@ -1,27 +1,37 @@
 package Meeting;
 
+import java.sql.Time;
 import java.util.Date;
+
 import mysql.sqlExecute;
 
 public class CreateMeeting {
 	
-	private int Meetingid;
-	private Date starttid;
-	private Date slutttid;
+	private String startTid;
+	private String sluttTid;
 	private String Beskrivelse;
-	
-	CreateMeeting(int Meetingid, Date starttid, Date slutttid, String Beskrivelse){
-		this.Meetingid = Meetingid;
-		this.starttid = starttid;
-		this.slutttid = slutttid;
+	private String room; 
+	private String date;
+	private int userID;
+	//private Time time;
+	private int moteid;
+	public CreateMeeting( String starttid, String slutttid, String Beskrivelse, String room, String date, int userID){
+		this.startTid = starttid;
+		this.sluttTid = slutttid;
 		this.Beskrivelse = Beskrivelse;
+		this.room=room;
+		this.date=date;
+		this.userID=userID;
+		
 	}
 	
-	private void create(){
+	public void create(){
 		sqlExecute cre = new sqlExecute();
-		cre.execute("INSERT INTO mote (moteid,starttidspunkt,sluttidspunkt,beskrivelse) VALUES ('" + Meetingid +
-				"','" + starttid + "','" + slutttid + "','" 
-	            + Beskrivelse + "')");
+		cre.execute("INSERT INTO mote (starttidspunkt,sluttidspunkt,beskrivelse,dato,opprettet_av) "
+				+ "VALUES ('" + startTid + "','" + sluttTid + "','" 
+	            + Beskrivelse + "','"+date+"','"+userID+"')");
+		//cre.execute("INSERT INTO mote_has_rom(mote_moteid,rom_romnavn) "
+			//	+ "VALUES('"  +"','"+room+"')");
 	}
 
 }
