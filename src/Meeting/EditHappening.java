c4097ba0c8badf875ef2ce2d880313d9d851959d
davidsl <davidsl@stud.ntnu.no>
2015-03-03 13:11:48 +0100

    Laget ferdig EditHappening

--- src/Meeting/EditHappening.java
+++ src/Meeting/EditHappening.java
@@ -1,5 +1,51 @@
 package Meeting;
+import mysql.sqlExecute;
 
-public class EditHappening {
+public class EditHappening {	
+	sqlExecute sql = new sqlExecute();
+	
+	private int hendelseid;
+	
+	EditHappening(int hendelseid) {
+		this.hendelseid = hendelseid;
+	}
+	
+	public void endreBeskrivelse(String Beskrivelse) {
+		
+		sql.execute("UPDATE hendelse SET hendelsebeskrivelse =" + "\"" + Beskrivelse + "\"" + "WHERE hendelseid =" + hendelseid);
+		
+	}
+	
+	public void endreStarttid(String starttid){
+		
+		sql.execute("UPDATE hendelse SET hendelsestart =" + "\"" + starttid + "\"" + "WHERE hendelseid =" + hendelseid);
+		
+	}
+	
+	public void endreSluttid(String sluttid){
+		
+		sql.execute("UPDATE hendelse SET hendelseslutt =" + "\"" + sluttid + "\"" + "WHERE hendelseid =" + hendelseid);
+		
+	}
 
+	public void endreDato(String dato){
+		
+		sql.execute("UPDATE hendelse SET hendelsedato =" + "\"" + dato + "\"" + "WHERE hendelseid =" + hendelseid);
+		
+	}
+	
+	public void endreSted(String nyttSted) {
+		
+		sql.execute("UPDATE hendelse SET hendelsested =" + "\"" + nyttSted + "\"" + "WHERE hendelseid ='" + hendelseid + "'");
+
+	}
+	
+			
+	public static void main(String[] args) {
+		EditHappening happening = new EditHappening(3);
+		
+		happening.endreSted("Skolen");
+		
+		
+	}
 }
