93c68f68dab107e4d7111756df5fd727412bc603
martinrholth <martinr.holth@gmail.com>
2015-03-03 09:56:10 +0100

    Change CheckRoom and add part of ChooseRoom in CreateMeeting

--- src/Meeting/CreateMeeting.java
+++ src/Meeting/CreateMeeting.java
@@ -2,6 +2,7 @@ package Meeting;
 
 import java.util.Scanner;
 
+import Room.CheckRoom;
 import mysql.sqlExecute;
 
 public class CreateMeeting {
@@ -13,22 +14,31 @@ public class CreateMeeting {
 	private String date;
 	private int userID;
 	private int moteid;
+	private int capacity;
 	
 	CreateMeeting() {
-		
+		this.userID=4;
 	}
 	
 	public void setMeeting(){
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Skriv inn start-tid for motet");
+		System.out.println("Skriv inn start-tid for motet: hh:mm:ss");
 		this.startTid = scanner.nextLine();
-		System.out.println("Skriv inn slutt-tid for motet");
+		System.out.println("Skriv inn slutt-tid for motet: hh:mm:ss");
 		this.sluttTid = scanner.nextLine();
 		System.out.println("Skriv inn en beskrivelse for motet");
 		this.Beskrivelse = scanner.nextLine();
-		System.out.println("Skriv inn dato for motet");
+		System.out.println("Skriv inn dato for motet: yyyy-mm-dd");
 		this.date= scanner.nextLine();
+		System.out.println("Hvor mange skal delta pa mote:");
+		this.capacity=Integer.parseInt(scanner.nextLine());
 
+	}
+	public void ChooseRoom(){
+		CheckRoom roomOption=new CheckRoom();
+		if(roomOption.check(this.date, this.startTid, this.sluttTid, this.capacity)){
+			//int choice
+		}
 		
 	}
 	
@@ -37,8 +47,15 @@ public class CreateMeeting {
 		cre.execute("INSERT INTO mote (starttidspunkt,sluttidspunkt,beskrivelse,dato,opprettet_av) "
 				+ "VALUES ('" + startTid + "','" + sluttTid + "','" 
 	            + Beskrivelse + "','"+date+"','"+userID+"')");
-		//cre.execute("INSERT INTO mote_has_rom(mote_moteid,rom_romnavn) "
-			//	+ "VALUES('"  +"','"+room+"')");
+		cre.execute("INSERT INTO mote_has_rom(mote_moteid,rom_romnavn) "
+				+ "VALUES('" +8 +"','"+"S4"+"')");
+	}
+	
+	public static void main(String[] args){
+		CreateMeeting meeting=new CreateMeeting();
+		meeting.setMeeting();
+		meeting.create();
 	}
 
 }
+
--- src/Room/CheckRoom.java
+++ src/Room/CheckRoom.java
@@ -6,27 +6,40 @@ import mysql.sqlExecute;
 import mysql.sqlRetrieve;
 
 public class CheckRoom {
+	public String [][]table;
 	
 	public boolean check(String dato, String starttid, String slutttid, int kapasitet) {
+
 		
 		sqlRetrieve sqlret = new sqlRetrieve("SELECT romnavn, beskrivelse, kapasitet, sted FROM rom WHERE romnavn NOT IN (SELECT r.romnavn"
 		+ " FROM mote m, mote_has_rom mr, rom r WHERE mr.mote_moteid = m.moteid "
 		+ "AND m.sluttidspunkt > '" + starttid + "' AND m.starttidspunkt < '" + slutttid + "' "
 		+ "AND m.dato = '" + dato + "' AND mr.rom_romnavn = r.romnavn) AND kapasitet >= " + kapasitet);
-		
+		table=sqlret.getQuery();
+		System.out.print(table[0][0]);
 		if (sqlret.getQuery().length == 0) {
 			System.out.println("Ingen ledige rom");
 			return false;
 		}
+		System.out.println("Valgnr - Romnavn   - Kapasitet - Sted             - Beskrivelse");
 		for (int i = 0; i < sqlret.getQuery().length; i++) {
-			for (int j = 0; j < sqlret.getQuery()[0].length; j++) {
-				System.out.println(sqlret.getQuery()[i][j]);
-			}
+			//for (int j = 0; j < sqlret.getQuery()[0].length; j++) {
+			 	System.out.print(i+"      -  ");
+				System.out.print(String.format("%-8s",sqlret.getQuery()[i][0]));
+				System.out.print(" - ");
+				System.out.print(String.format("%-9s",sqlret.getQuery()[i][2]));
+				System.out.print(" - ");
+				System.out.print(String.format("%-16s",sqlret.getQuery()[i][3]));
+				System.out.print(" - ");
+				System.out.print(String.format("%-16s",sqlret.getQuery()[i][1]));
+				System.out.println();
+				//String.format("%40s",")
 		}
 		
 		return true;
 		
 	}
+	
 	
 	public static void main(String[] args) {
 		CheckRoom rom = new CheckRoom();
