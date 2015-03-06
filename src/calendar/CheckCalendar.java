48dfe784214614f4fa8feceedfa18e921e67679e
BrageD <brage.dimm@hotmail.com>
2015-03-06 14:49:00 +0100

    Programmet kræsjer ikke dersom man velger noen som har møter uten rom!

--- src/calendar/CheckCalendar.java
+++ src/calendar/CheckCalendar.java
@@ -7,10 +7,25 @@ public class CheckCalendar {
 	//sqlRetrieve info = new sqlRetrieve();
 	public static String PrintDay(int brukerid){
 		String str;
-		sqlRetrieve info = new sqlRetrieve("SELECT * FROM mote m, mote_has_bruker mb, mote_has_rom mr "
-				+ "WHERE mb.bruker_brukerid="+brukerid+
-				" AND mr.mote_moteid = m.moteid AND mb.mote_moteid = m.moteid ORDER BY dato, starttidspunkt ASC");
-		
+		sqlRetrieve info = new sqlRetrieve("(SELECT * FROM(SELECT moteid,dato, starttidspunkt,sluttidspunkt,"
+				+ " null as romnavn, beskrivelse, sted"
+				+ " FROM mote m1, mote_has_bruker mb1 WHERE m1.moteid NOT IN"
+				+ " (SELECT moteid FROM mote m2, mote_has_bruker mb2, mote_has_rom mr2 "
+				+ " WHERE mb2.bruker_brukerid= " + brukerid + " AND mr2.mote_moteid = m2.moteid "
+				+ " AND mb2.mote_moteid = m2.moteid) AND m1.moteid = mb1.mote_moteid"
+				+ " AND mb1.bruker_brukerid = " + brukerid +") AS temp1)"
+				+ " UNION "
+				+"	(SELECT * FROM(SELECT m3.moteid, m3.dato, m3.starttidspunkt, m3.sluttidspunkt, mr3.rom_romnavn, m3.beskrivelse, m3.sted"
+				+ "	FROM mote m3, mote_has_bruker mb3,mote_has_rom mr3 "
+				+ "	WHERE mb3.bruker_brukerid= " + brukerid
+				+ " AND mr3.mote_moteid = m3.moteid"
+				+ "	AND mb3.mote_moteid = m3.moteid"
+				+ "	ORDER BY dato, starttidspunkt ASC)"
+				+ "	AS temp2)"
+				+ " ORDER BY dato, starttidspunkt ASC");
+
+
+			
 		
 		str = ("Date       - Start    - End      - Description - Room");
 		sqlRetrieve moter = new sqlRetrieve ("SELECT COUNT(* )FROM mote_has_bruker WHERE bruker_brukerid = " + "\"" + brukerid + "\"");
@@ -18,8 +33,8 @@ public class CheckCalendar {
 		
 		
 		for( int i=0; i < Integer.parseInt(moter.getQuery()[0][0]); i++){
-			str += "\n" + (info.getQuery()[i][4] +" - "+info.getQuery()[i][1]+" - "
-		+ info.getQuery()[i][2]+" - "+info.getQuery()[i][3]+" - "+info.getQuery()[i][12]);
+			str += "\n" + (info.getQuery()[i][1] +" - "+info.getQuery()[i][2]+" - "
+		+ info.getQuery()[i][3]+" - "+info.getQuery()[i][5]+" - "+info.getQuery()[i][4] + " - " + info.getQuery()[i][6]);
 		}
 		System.out.println(str);
 		return str;
@@ -28,7 +43,7 @@ public class CheckCalendar {
 
 public static void main(String[] args) {
 	CheckCalendar test=new CheckCalendar();
-	test.PrintDay(6);
+	test.PrintDay(5);
 	}
 }
 
