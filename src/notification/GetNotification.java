5c635512a4dbbe7c588c7f95e10a261131945c33
BrageD <brage.dimm@hotmail.com>
2015-03-09 16:10:56 +0100

    GetNotification returnerer nå notifications som en String

--- src/notification/GetNotification.java
+++ src/notification/GetNotification.java
@@ -5,22 +5,26 @@ import mysql.sqlRetrieve;
 public class GetNotification {
 	
 	private int brukerid;
+	public String out = "";
 	
 	GetNotification(int brukerid){
 		this.brukerid = brukerid;
 	}
 	
-	public void get(){
+	public String get(){
 		
 		sqlRetrieve notifications = new sqlRetrieve(("SELECT beskrivelse, time_stamp FROM notification WHERE bruker_brukerid = '" +  brukerid +"'"));
 		for (int i = 0; i < notifications.getQuery().length; i++) {
-			System.out.println(String.format("%-60s", notifications.getQuery()[i][0]) + "        -  "+
-					String.format("%-8s",notifications.getQuery()[i][1]));
+			out += String.format(" %-60s", notifications.getQuery()[i][0]) + "        -  " +
+					String.format("%-8s %n",notifications.getQuery()[i][1] );
+			
+			
 		}
+		return out;
 	}
 	
 	public static void main(String[] args) {
 		GetNotification Notif = new GetNotification(5);
-		Notif.get();
+		System.out.println(Notif.get());
 	}
 }
